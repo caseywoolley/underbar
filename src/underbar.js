c34734cc6da0a14d242b6ee5b512573fdf884551
Casey Woolley <caseywoolley@gmail.com>
2015-09-01 05:16:19 -0500

    fixed extend

--- src/underbar.js
+++ src/underbar.js
@@ -230,6 +230,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+      return _.reduce(arguments, function(newObj, argObj){
+        _.each(argObj, function(prop, key){
+          newObj[key] = argObj[key];
+        });
+        return newObj;
+      }, obj);
   };
 
   // Like extend, but doesn't ever overwrite a key that already
